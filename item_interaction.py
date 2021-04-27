aa4c1617cb71178f956a5ac0a694a207d893090d
tastytypist <nathanael310802@gmail.com>
2021-04-28 01:47:20 +0700

    Add return value for functions and procedures in item_interaction and database_io

--- item_interaction.py
+++ item_interaction.py
@@ -18,43 +18,45 @@
 def add_item(gadget_database, consumable_database):
     rarity_list = ("S", "A", "B", "C")
 
-    item_id = input("Input gadget ID: ")
+    item_id = input("Masukkan ID item: ")
 
     # Checks membership of item_id in every sublist of gadget_database and consumable_database
     if any(item_id in _ for _ in gadget_database) or any(item_id in _ for _ in consumable_database):
-        print("Add item failed, item already existed!")
+        print("Penambahan item gagal. Item sudah ada!")
     elif item_id[0] not in ("G", "C"):  # Item ID is invalid
-        print("Add item failed, item ID is invalid!")
+        print("Penambahan item gagal. ID item invalid!")
     elif item_id[0] == "G":  # Item is a gadget
-        gad_name = input("Input gadget name: ")
-        gad_desc = input("Input gadget description: ")
-        gad_amount = int(input("Input amount of gadget: "))
+        gad_name = input("Masukkan nama gadget: ")
+        gad_desc = input("Masukkan deskripsi gadget: ")
+        gad_amount = int(input("Masukkan jumlah gadget: "))
         if gad_amount <= 0:
-            print("Add gadget failed, gadget amount is invalid!")
+            print("Penambahan gadget gagal. Jumlah gadget invalid!")
         else:
-            gad_rarity = input("Input gadget rarity: ")
+            gad_rarity = input("Masukkan rarity gadget: ")
             if gad_rarity not in rarity_list:
-                print("Add gadget failed, gadget rarity is invalid!")
+                print("Penambahan gadget gagal. Rarity gadget invalid!")
             else:
-                gad_year = int(input("Input gadget found year: "))
+                gad_year = int(input("Masukkan tahun penemuan gadget: "))
                 if gad_year <= 0:
-                    print("Add gadget failed, gadget year is invalid!")
+                    print("Penambahan gadget gagal. Tahun penemuan gadget invalid!")
                 else:
                     new_gadget = [item_id, gad_name, gad_desc, gad_amount, gad_rarity, gad_year]
                     gadget_database.append(new_gadget)
     else:  # Item is a consumable
-        con_name = input("Input consumable name: ")
-        con_desc = input("Input consumable description: ")
-        con_amount = int(input("Input amount of consumable: "))
+        con_name = input("Masukkan nama consumable: ")
+        con_desc = input("Masukkan deskripsi consumable: ")
+        con_amount = int(input("Masukkan jumlah consumable: "))
         if con_amount <= 0:
-            print("Add consumable failed, consumable amount is invalid!")
+            print("Penambahan consumable gagal. Jumlah consumable invalid!")
         else:
-            con_rarity = input("Input consumable rarity: ")
+            con_rarity = input("Masukkan rarity consumable: ")
             if con_rarity not in rarity_list:
-                print("Add consumable failed, consumable rarity is invalid!")
+                print("Penambahan consumable gagal. Rarity consumable invalid!")
             else:
                 new_consumable = [item_id, con_name, con_desc, con_amount, con_rarity]
                 consumable_database.append(new_consumable)
+
+    return gadget_database, consumable_database
 
 
 # Function delete_item
@@ -67,29 +69,46 @@ def add_item(gadget_database, consumable_database):
 
 # Algorithm
 def delete_conf(database, item_index):
-    confirm = input(f"Are you sure to delete {database[item_index][1]} (y/n)? ")
+    confirm = input(f"Apakah Anda yakin ingin menghapus {database[item_index][1]} (y/n)? ")
     if confirm == "y":
         del database[item_index]
-        print("Item has been successfully removed!")
+        print("Item telah berhasil dihapus!")
+        return database, 1
     else:
-        pass  # Program does nothing if user selects "n"
+        return [], 0  # Program indicates nothing happened if user selects "n"
 
 
 def delete_item(gadget_database, consumable_database):
-    item_id = input("Input gadget ID: ")
+    modified_gadget_database = []
+    modified_consumable_database = []
+    gadget_modified = 0
+    consum_modified = 0
+
+    item_id = input("Masukkan ID item: ")
 
     # Checks membership of item_id in gadget_database
     if any(item_id in _ for _ in gadget_database):
         # Unpacks the nested list in order to be indexed
         item_index = [item_id for [item_id, _, _, _, _, _] in gadget_database].index(item_id)
-        delete_conf(gadget_database, item_index)
+        return_list, tracker = delete_conf(gadget_database, item_index)
+        modified_gadget_database += return_list
+        gadget_modified += tracker
     # Checks membership of item_id in consumable_database
     elif any(item_id in _ for _ in consumable_database):
         # Unpacks the nested list in order to be indexed
         item_index = [item_id for [item_id, _, _, _, _] in consumable_database].index(item_id)
-        delete_conf(consumable_database, item_index)
+        return_list, tracker = delete_conf(consumable_database, item_index)
+        modified_consumable_database += return_list
+        consum_modified += tracker
     else:  # item_id isn't found
-        print("Delete item failed. Item doesn't exist!")
+        print("Penghapusan item gagal. Item tidak ditemukan!")
+
+    if gadget_modified:
+        return modified_gadget_database, consumable_database
+    elif consum_modified:
+        return gadget_database, modified_consumable_database
+    else:
+        return gadget_database, consumable_database
 
 
 # Function modify_item_amount
@@ -102,40 +121,58 @@ def delete_item(gadget_database, consumable_database):
 
 # Algorithm
 def amount_check(database, item_index):
-    amount = int(input("Input amount to modify: "))
+    amount = int(input("Masukkan perubahan jumlah: "))
 
     # Modifies the gadget amount accordingly
     if database[item_index][3] + amount >= 0:
         database[item_index][3] += amount
-        print(f"{amount} {database[item_index][1]} has been added. "
-              f"Current amount: {database[item_index][3]}")
+        print(f"{amount} {database[item_index][1]} telah ditambahkan. "
+              f"Jumlah sekarang: {database[item_index][3]}")
+        return database, 1
     else:
-        print(f"Modify amount of {database[item_index][1]} failed due to insufficient item amount. "
-              f"Current amount: {database[item_index][3]}")
+        print(f"Perubahan jumlah {database[item_index][1]} gagal karena kurangnya jumlah item. "
+              f"Jumlah sekarang: {database[item_index][3]}")
+        return database, 0
 
 
 def modify_item_amount(gadget_database, consumable_database):
-    item_id = input("Input item ID: ")
+    modified_gadget_database = []
+    modified_consumable_database = []
+    gadget_modified = 0
+    consum_modified = 0
+
+    item_id = input("Masukkan ID item: ")
 
     if any(item_id in _ for _ in gadget_database):
         # Unpacks the nested list in order to be indexed
         item_index = [item_id for [item_id, _, _, _, _, _] in gadget_database].index(item_id)
-        amount_check(gadget_database, item_index)
+        return_list, tracker = amount_check(gadget_database, item_index)
+        modified_gadget_database += return_list
+        gadget_modified += tracker
     elif any(item_id in _ for _ in consumable_database):
         # Unpacks the nested list in order to be indexed
         item_index = [item_id for [item_id, _, _, _, _] in consumable_database].index(item_id)
-        amount_check(consumable_database, item_index)
+        return_list, tracker = amount_check(consumable_database, item_index)
+        modified_consumable_database += return_list
+        consum_modified += tracker
+    else:  # item_id isn't found
+        print("Perubahan jumlah item gagal. Item tidak ditemukan!")
+
+    if gadget_modified:
+        return modified_gadget_database, consumable_database
+    elif consum_modified:
+        return gadget_database, modified_consumable_database
     else:
-        print("Modify item amount failed. Item doesn't exist!")
+        return gadget_database, consumable_database
 
 
 # Use these for testing!
-gadget = [["G5", "Car key", "Stolen for the lost boys.", 1, "A", 2010],
-          ["G6", "Pocket knife", "Carved your name with it!", 1, "B", 2014]]
-consum = [["C5", "Jumpsuit", "If you need anyone..", 1, "A"],
-          ["C4", "Strawberry swing", "Wouldn't want to waste a thing..", 2, "S"]]
+# gadget = [["G5", "Car key", "Stolen for the lost boys.", 1, "A", 2010],
+#           ["G6", "Pocket knife", "Carved your name with it!", 1, "B", 2014]]
+# consum = [["C5", "Jumpsuit", "If you need anyone..", 1, "A"],
+#           ["C4", "Strawberry swing", "Wouldn't want to waste a thing..", 2, "S"]]
 
-if __name__ == "__main__":
-    modify_item_amount(gadget, consum)
-    print(f"Gadget : {gadget}")
-    print(f"Consum : {consum}")
+# if __name__ == "__main__":
+#     modify_item_amount(gadget, consum)
+#     print(f"Gadget : {gadget}")
+#     print(f"Consum : {consum}")
