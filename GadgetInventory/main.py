e8e8fed7459f30fb3eb9f74fcb75b41033afff07
tastytypist <nathanael310802@gmail.com>
2021-05-01 02:31:14 +0700

    Add several features:
    - module import has been completed
    - add support for gadget_log
    - remove Doraemon logo
    - add role-based command availability validation
    - add stuff here and there
    - clean stuff here and there

--- GadgetInventory/main.py
+++ GadgetInventory/main.py
@@ -3,15 +3,13 @@
 
 # Dependencies
 import argparse
-import database_io
 import userinfo
 import cari_item
 import item_interaction
-import demand_Gadget_Consumable
+import item_movement
 import riwayat_gadget
+import database_io
 import help_log
-
-
 
 # Dictionary
 #
@@ -20,13 +18,14 @@ import help_log
 parser = argparse.ArgumentParser()
 parser.add_argument("foldername", help="load a folder with the specified name", type=str)
 folder = parser.parse_args()
-consumable_raw, consumable_history_raw, gadget_raw,\
-    gadget_borrow_history_raw, gadget_return_history_raw, user_raw = database_io.load(folder.foldername)
+consumable_raw, consumable_history_raw, gadget_raw, gadget_borrow_history_raw, \
+    gadget_log_raw, gadget_return_history_raw, user_raw = database_io.load(folder.foldername)
 
 consum_header = consumable_raw.pop(0)
 consum_hist_header = consumable_history_raw.pop(0)
 gadget_header = gadget_raw.pop(0)
 gadget_borrow_header = gadget_borrow_history_raw.pop(0)
+gadget_log_header = gadget_log_raw.pop(0)
 gadget_return_header = gadget_return_history_raw.pop(0)
 user_header = user_raw.pop(0)
 
@@ -34,6 +33,7 @@ consumable = database_io.change_data_type("consum", consumable_raw)
 consumable_history = database_io.change_data_type("consum_history", consumable_history_raw)
 gadget = database_io.change_data_type("gadget", gadget_raw)
 gadget_borrow_history = database_io.change_data_type("gadget_borrow", gadget_borrow_history_raw)
+gadget_log = database_io.change_data_type("gadget_log", gadget_log_raw)
 gadget_return_history = database_io.change_data_type("gadget_return", gadget_return_history_raw)
 user = database_io.change_data_type("user", user_raw)
 
@@ -41,18 +41,24 @@ consum_data = []
 consum_hist_data = []
 gadget_data = []
 gadget_borrow_hist_data = []
+gadget_log_data = []
 gadget_return_hist_data = []
 user_data = []
 
 id_pinjam, id_kembalian, id_minta = 0, 0, 0
+username = ""
+role = ""
 
 
-def save_csv(consum_datas, consum_hist_datas, gadget_datas,
-             gadget_borrow_hist_datas, gadget_return_hist_datas, user_datas):
+def save_csv(consum_datas, consum_hist_datas, gadget_datas, gadget_borrow_hist_datas,
+             gadget_log_datas, gadget_return_hist_datas, user_datas):
+    folder_name = input("Masukkan nama folder penyimpanan: ")
+
     consum_datas.append(consum_header)
     consum_hist_datas.append(consum_hist_header)
     gadget_datas.append(gadget_header)
     gadget_borrow_hist_datas.append(gadget_borrow_header)
+    gadget_log_datas.append(gadget_log_header)
     gadget_return_hist_datas.append(gadget_return_header)
     user_datas.append(user_header)
 
@@ -60,6 +66,7 @@ def save_csv(consum_datas, consum_hist_datas, gadget_datas,
     consum_hist_datas += consumable_history
     gadget_datas += gadget
     gadget_borrow_hist_datas += gadget_borrow_history
+    gadget_log_datas += gadget_log
     gadget_return_hist_datas += gadget_return_history
     user_datas += user
 
@@ -67,6 +74,7 @@ def save_csv(consum_datas, consum_hist_datas, gadget_datas,
     consum_hist_csv = database_io.revert_data_type(consum_hist_datas)
     gadget_csv = database_io.revert_data_type(gadget_datas)
     gadget_borrow_csv = database_io.revert_data_type(gadget_borrow_hist_datas)
+    gadget_log_csv = database_io.revert_data_type(gadget_log_datas)
     gadget_return_csv = database_io.revert_data_type(gadget_return_hist_datas)
     user_csv = database_io.revert_data_type(user_datas)
 
@@ -74,61 +82,94 @@ def save_csv(consum_datas, consum_hist_datas, gadget_datas,
     database_io.save(folder_name, "consumable_history.csv", consum_hist_csv)
     database_io.save(folder_name, "gadget.csv", gadget_csv)
     database_io.save(folder_name, "gadget_borrow_history.csv", gadget_borrow_csv)
+    database_io.save(folder_name, "gadget_log.csv", gadget_log_csv)
     database_io.save(folder_name, "gadget_return_history.csv", gadget_return_csv)
     database_io.save(folder_name, "user.csv", user_csv)
 
+    print(f"Data telah disimpan di folder {folder_name}.")
+
 
 print("Selamat datang ke sistem penyimpanan item Doremonangis!")
-print("""  
-            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣴⣶⣶⣶⣶⣶⠶⣶⣤⣤⣀⠀⠀⠀⠀⠀⠀
-            ⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⠁⠀⢀⠈⢿⢀⣀⠀⠹⣿⣿⣿⣦⣄⠀⠀⠀
-            ⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⠿⠀⠀⣟⡇⢘⣾⣽⠀⠀⡏⠉⠙⢛⣿⣷⡖⠀
-            ⠀⠀⠀⠀⠀⣾⣿⣿⡿⠿⠷⠶⠤⠙⠒⠀⠒⢻⣿⣿⡷⠋⠀⠴⠞⠋⠁⢙⣿⣄
-            ⠀⠀⠀⠀⢸⣿⣿⣯⣤⣤⣤⣤⣤⡄⠀⠀⠀⠀⠉⢹⡄⠀⠀⠀⠛⠛⠋⠉⠹⡇
-            ⠀⠀⠀⠀⢸⣿⣿⠀⠀⠀⣀⣠⣤⣤⣤⣤⣤⣤⣤⣼⣇⣀⣀⣀⣛⣛⣒⣲⢾⡷
-            ⢀⠤⠒⠒⢼⣿⣿⠶⠞⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⣼⠃
-            ⢮⠀⠀⠀⠀⣿⣿⣆⠀⠀⠻⣿⡿⠛⠉⠉⠁⠀⠉⠉⠛⠿⣿⣿⠟⠁⠀⣼⠃⠀
-            ⠈⠓⠶⣶⣾⣿⣿⣿⣧⡀⠀⠈⠒⢤⣀⣀⡀⠀⠀⣀⣀⡠⠚⠁⠀⢀⡼⠃⠀⠀
-            ⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣷⣤⣤⣤⣤⣭⣭⣭⣭⣭⣥⣤⣤⣤⣴⣟⠁
-""")
 
 while True:
     command = input("Masukkan perintah: ")
 
     if command == "reg":
-        user , role = userinfo.register(user)
+        if role == "admin":
+            user = userinfo.register(user)
+        else:
+            print("Maaf, perintah ini hanya tersedia untuk admin program.")
     elif command == "log":
-        userinfo.login(user)
+        username, role = userinfo.login(user)
     elif command == "crare":
-        cari_item.carirarity(gadget)
+        if not role:
+            print("Silakan login terlebih dahulu untuk menggunakan perintah ini (perintah : login).")
+        else:
+            cari_item.carirarity(gadget)
     elif command == "cyear":
-        cari_item.caritahun(gadget)
+        if not role:
+            print("Silakan login terlebih dahulu untuk menggunakan perintah ini (perintah : login).")
+        else:
+            cari_item.caritahun(gadget)
     elif command == "add":
-        gadget, consumable = item_interaction.add_item(gadget, consumable)
+        if role == "admin":
+            gadget, consumable = item_interaction.add_item(gadget, consumable)
+        else:
+            print("Maaf, perintah ini hanya tersedia untuk admin program.")
     elif command == "delete":
-        gadget, consumable = item_interaction.delete_item(gadget, consumable)
+        if role == "admin":
+            gadget, consumable = item_interaction.delete_item(gadget, consumable)
+        else:
+            print("Maaf, perintah ini hanya tersedia untuk admin program.")
     elif command == "change":
-        gadget, consumable = item_interaction.modify_item_amount(gadget, consumable)
+        if role == "admin":
+            gadget, consumable = item_interaction.modify_item_amount(gadget, consumable)
+        else:
+            print("Maaf, perintah ini hanya tersedia untuk admin program.")
     elif command == "borrow":
-        gadget, buku_hutang_li, gadget_borrow_history, ID_pinjam = demand_Gadget_Consumable.pinjam(user_name, gadget, buku_hutang_li, gadget_borrow_history, ID_pinjam)
+        if role == "user":
+            gadget, gadget_log, gadget_borrow_history, id_pinjam = item_movement.pinjam(username, gadget, gadget_log,
+                                                                                        gadget_borrow_history,
+                                                                                        id_pinjam)
+        else:
+            print("Maaf, perintah ini hanya tersedia untuk user. Silakan login sebagai user (perintah : login).")
     elif command == "return":
-        gadget, buku_hutang_li, gadget_return_history, ID_kembalian = demand_Gadget_Consumable.kembalikan(user_name, gadget, buku_hutang_li, gadget_return_history, ID_kembalian)
+        if role == "user":
+            gadget, gadget_log, gadget_return_history, id_kembalian = item_movement.kembalikan(username, gadget,
+                                                                                               gadget_log,
+                                                                                               gadget_return_history,
+                                                                                               id_kembalian)
+        else:
+            print("Maaf, perintah ini hanya tersedia untuk user. Silakan login sebagai user (perintah : login).")
     elif command == "demand":
-        consumable, consumable_history, ID_minta = demand_Gadget_Consumable.minta(user_name, consumable, consumable_history, ID_minta)
+        if role == "user":
+            consumable, consumable_history, id_minta = item_movement.minta(username, consumable, consumable_history,
+                                                                           id_minta)
+        else:
+            print("Maaf, perintah ini hanya tersedia untuk user. Silakan login sebagai user (perintah : login).")
     elif command == "hborrow":
-        riwayat_gadget.riwayatPinjamGadget(gadget_borrow_history, user, gadget)
+        if role == "admin":
+            riwayat_gadget.riwayatPinjamGadget(gadget_borrow_history, user, gadget)
+        else:
+            print("Maaf, perintah ini hanya tersedia untuk admin program.")
     elif command == "hreturn":
-        riwayat_gadget.riwayatBalikGadget(gadget_return_history, user, gadget, gadget_borrow_history)
+        if role == "admin":
+            riwayat_gadget.riwayatBalikGadget(gadget_return_history, user, gadget, gadget_borrow_history)
+        else:
+            print("Maaf, perintah ini hanya tersedia untuk admin program.")
     elif command == "hdemand":
-        riwayat_gadget.riwayatConsumable(consumable_history, user, consumable)
+        if role == "admin":
+            riwayat_gadget.riwayatConsumable(consumable_history, user, consumable)
+        else:
+            print("Maaf, perintah ini hanya tersedia untuk admin program.")
     elif command == "save":
-        folder_name = input("Masukkan nama folder penyimpanan: ")
-
-        save_csv(consum_data, consum_hist_data, gadget_data,
-                 gadget_borrow_hist_data, gadget_return_hist_data, user_data)
-        print(f"Data telah disimpan di folder {folder_name}.")
+        if not role:
+            save_csv(consum_data, consum_hist_data, gadget_data, gadget_borrow_hist_data,
+                     gadget_log_data, gadget_return_hist_data, user_data)
+        else:
+            print("Silakan login terlebih dahulu untuk menggunakan perintah ini (perintah : login).")
     elif command == "help":
-        pass
+        help_log.help(role)
     elif command == "exit":
         do_save = input("Apakah Anda ingin melakukan penyimpanan file?: (y/n) ").upper()
 
@@ -136,11 +177,11 @@ while True:
             print("Masukan tidak valid. Harap ulangi!")
             continue
         elif do_save == "Y":
-            save_csv(consum_data, consum_hist_data, gadget_data,
-                     gadget_borrow_hist_data, gadget_return_hist_data, user_data)
+            save_csv(consum_data, consum_hist_data, gadget_data, gadget_borrow_hist_data,
+                     gadget_log_data, gadget_return_hist_data, user_data)
         else:
             pass
 
         break
     else:
-        print("Command tidak valid! Harap ulangi kembali.")
+        print("Perintah tidak valid! Gunakan perintah 'help' untuk melihat daftar perintah yang tersedia.")
